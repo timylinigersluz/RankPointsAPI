122557530762d88de318ed9ce55ec307860d2081
Samuel Huwiler <Samuel_huwiler@sluz.ch>
2025-01-22 12:55:53 +0100

    Add Comments to the top-level RangAPI class

--- src/main/java/ch/ksrminecraft/RangAPI.java
+++ src/main/java/ch/ksrminecraft/RangAPI.java
@@ -15,14 +15,25 @@ public class RangAPI {
         this.api = new PointsAPI(con);
     }
 
+    /*
+    Set the points from a specific Player to any given amount
+    Not recommended for regular Use. Use addPoints instead
+     */
     public void setPoints(UUID uuid, int points){
         api.SQLUpdate("Update points set points = " + points + " where UUID = ' " + uuid  + "'");
     }
+    /*
+    Add a number of Points to the total Score of a specified Player
+    delta can be negative for points subtraction
+     */
     public void addPoints(UUID uuid, int delta){
         int points = api.SQLgetInt("Select points from points where UUID = ' " + uuid + "'");
         int newPoints = points + delta;
         api.SQLUpdate("Update points set points = " + newPoints + " where UUID = ' " + uuid + "'");
     }
+    /*
+    Returns the current Points score from a given Player
+     */
     public int getPoints(UUID uuid){
         int points = api.SQLgetInt("Select points from points where UUID = ' " + uuid + "'");
         return points;
